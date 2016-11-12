f2bc1c1082c7727c640a20e983fcb865f468f730
Joe Simpson <joesimpsonjr@gmail.com>
2016-11-12 09:45:19 -0800

    Final Project

--- wk10_finalproject/js/index.js
+++ wk10_finalproject/js/index.js
@@ -13,3 +13,17 @@ $(function() {
     }
   });
 });
+
+/* Counter */
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
